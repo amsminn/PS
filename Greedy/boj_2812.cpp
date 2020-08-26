4fc94812471998cb70ccbb1acea800a730ee1061
kim1109123 <kim1109123@github.com>
2020-08-26 22:10:14 +0900

    com1

--- Greedy/boj_2812.cpp
+++ Greedy/boj_2812.cpp
@@ -13,7 +13,7 @@ int main() {
 	}
 	sort(srt.begin(), srt.end(), [](int a, int b) {return a > b; }); //srt를 내림차순으로 정렬
 	//출력할 수열의 길이는 n-k이고 n-k번째로 큰 수가 수열에 들어감
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) { 
 		for (int j = 0; j < n - k; j++)	
 			//n-k번째로 큰 수까지 수열에 들어갈 수 있기 때문에 
 			//arr[i]가 srt[0]부터 srt[n-k-1] 사이에있다면 수열에 포함
