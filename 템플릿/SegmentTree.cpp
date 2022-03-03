2e0286c71371ae51439fd7d0aef003ecef5c0c50
kim1109123 <66085474+kim1109123@users.noreply.github.com>
2022-03-04 02:46:06 +0900

    Update SegmentTree.cpp

--- 템플릿/SegmentTree.cpp
+++ 템플릿/SegmentTree.cpp
@@ -3,16 +3,17 @@ struct seg{
     // 0 based index
     int n;T identity;vector<T>tree;
     function<T(T,T)>op;
+    seg() {}
     seg(int _n,T _identity,function<T(T,T)>_op)
-        :n(_n),identity(_identity),op(_op){tree.resize(2*_n);}
+        :n(_n),identity(_identity),op(_op){tree=vector<T>(2*_n,identity);}
     void upd(int i,T x){
         tree[i+=n]=x;
-        for(i>>=1;i>=1;i>>=1)tree[i]=op(tree[i<<1],tree[i<<1|1]);
+        for(i>>=1;i>0;i>>=1)tree[i]=op(tree[i<<1],tree[i<<1|1]);
     }
     T qry(int l,int r){
         T ret=identity;
         for(l+=n,r+=n;l<=r;l>>=1,r>>=1){
-            if(l&1)ret=op(ret,tree[l++]);
+            if(l&1)ret=op(tree[l++], ret);
             if(~r&1)ret=op(ret,tree[r--]);
         }
         return ret;
