4eac2de831c2746152e4dd54c7aeec56409a2478
kim1109123 <66085474+kim1109123@users.noreply.github.com>
2023-03-29 10:11:01 +0900

    Update SegmentTree.cpp

--- 템플릿/SegmentTree.cpp
+++ 템플릿/SegmentTree.cpp
@@ -11,11 +11,11 @@ struct seg{
         for(i>>=1;i>0;i>>=1)tree[i]=op(tree[i<<1],tree[i<<1|1]);
     }
     T qry(int l,int r){
-        T ret=ID;
+        T retL=ID,retR=ID;
         for(l+=n,r+=n;l<=r;l>>=1,r>>=1){
-            if(l&1)ret=op(tree[l++], ret);
-            if(~r&1)ret=op(ret,tree[r--]);
+            if(l&1)retL=op(tree[l++], retL);
+            if(~r&1)retR=op(retR,tree[r--]);
         }
-        return ret;
+        return op(retL,retR);
     }
 };
