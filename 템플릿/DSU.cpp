c45e0e76f45858db2bb424a96ef0a9abc431cd15
kim1109123 <66085474+kim1109123@users.noreply.github.com>
2023-03-29 10:12:24 +0900

    Update DSU.cpp

--- 템플릿/DSU.cpp
+++ 템플릿/DSU.cpp
@@ -1,11 +1,4 @@
 struct DSU{
-    vector<int>pa;int n;
-    DSU(int _n=0):n(_n){pa.resize(n+1);iota(pa.begin(),pa.end(),0);}
-    int find(int a){return pa[a]==a?a:(pa[a]=find(pa[a]));}
-    void merge(int a,int b){pa[find(b)]=find(a);}
-};
-
-struct DSU{
     vector<int>pa,sz;int n;
     DSU(int _n=0):n(_n){
         pa.resize(n+1);sz=vector<int>(n+1,1);
